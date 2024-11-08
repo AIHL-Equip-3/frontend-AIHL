eb38d7e47f9c2671331d63c4e108f3a482bae6ce
paugarcia32 <paugarcia32@gmail.com>
2024-11-09 00:59:57 +0100

    prant de cosas

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class LoginWebView extends StatefulWidget {
   const LoginWebView({super.key});
@@ -10,72 +10,8 @@ class LoginWebView extends StatefulWidget {
 }
 
 class _LoginWebViewState extends State<LoginWebView> {
-  late final WebViewController _controller;
+  late InAppWebViewController _webViewController;
   String? token;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..addJavaScriptChannel(
-        'TokenChannel',
-        onMessageReceived: (JavaScriptMessage message) {
-          String responseText = message.message;
-          print('Respuesta recibida: $responseText');
-          try {
-            Map<String, dynamic> jsonResponse = jsonDecode(responseText);
-            token = jsonResponse['token'];
-            print('Token: $token');
-            // Puedes usar el token aquí
-          } catch (e) {
-            print('Error al decodificar la respuesta: $e');
-          }
-        },
-      )
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (String url) {
-            _injectJavaScript();
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(
-          'https://valid.aoc.cat/o/oauth2/auth?lang=ca&scope=autenticacio_usuari&redirect_uri=https://ovt.gencat.cat/carpetaciutadana360/AppJava/api/login&response_type=code&client_id=gsit.gencat.cat&approval_prompt=auto&state=inici_set-locale=ca_ES'));
-  }
-
-  void _injectJavaScript() {
-    String js = """
-      (function() {
-        // Interceptar XMLHttpRequest
-        var open = XMLHttpRequest.prototype.open;
-        XMLHttpRequest.prototype.open = function(method, url) {
-          this.addEventListener('load', function() {
-            if (url.includes('/carpetaciutadana360/mfe-main-app/AppJava/api/token')) {
-              TokenChannel.postMessage(this.responseText);
-            }
-          });
-          open.apply(this, arguments);
-        };
-
-        // Interceptar fetch
-        var originalFetch = window.fetch;
-        window.fetch = function() {
-          return originalFetch.apply(this, arguments).then(function(response) {
-            if (response.url.includes('/carpetaciutadana360/mfe-main-app/AppJava/api/token')) {
-              response.clone().text().then(function(bodyText) {
-                TokenChannel.postMessage(bodyText);
-              });
-            }
-            return response;
-          });
-        };
-      })();
-    """;
-
-    _controller.runJavaScript(js);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -83,7 +19,69 @@ class _LoginWebViewState extends State<LoginWebView> {
       appBar: AppBar(
         title: const Text('Iniciar Sesión'),
       ),
-      body: WebViewWidget(controller: _controller),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+          url: WebUri(
+              'https://valid.aoc.cat/o/oauth2/auth?lang=ca&scope=autenticacio_usuari&redirect_uri=https://ovt.gencat.cat/carpetaciutadana360/AppJava/api/login&response_type=code&client_id=gsit.gencat.cat&approval_prompt=auto&state=inici_set-locale=ca_ES'),
+        ),
+        initialSettings: InAppWebViewSettings(
+          useShouldInterceptAjaxRequest: true,
+          useShouldInterceptFetchRequest: true,
+        ),
+        onWebViewCreated: (controller) {
+          _webViewController = controller;
+        },
+        shouldInterceptAjaxRequest: (controller, ajaxRequest) async {
+          if (ajaxRequest.url.toString().contains(
+              '/carpetaciutadana360/mfe-main-app/AppJava/api/token')) {
+            print('Ajax request intercepted: ${ajaxRequest.url}');
+            _webViewController.addJavaScriptHandler(
+              handlerName: 'ajaxHandler',
+              callback: (args) async {
+                var ajaxResponse = args[0];
+                print('Ajax response received: $ajaxResponse');
+                try {
+                  Map<String, dynamic> jsonResponse = jsonDecode(ajaxResponse);
+                  token = jsonResponse['token'];
+                  print('Token captured: $token');
+                } catch (e) {
+                  print('Error decoding response: $e');
+                }
+                return ajaxResponse;
+              },
+            );
+          }
+          return ajaxRequest;
+        },
+        shouldInterceptFetchRequest: (controller, fetchRequest) async {
+          if (fetchRequest.url.toString().contains(
+              '/carpetaciutadana360/mfe-main-app/AppJava/api/token')) {
+            print('Fetch request intercepted: ${fetchRequest.url}');
+            controller.addJavaScriptHandler(
+              handlerName: 'fetchHandler',
+              callback: (args) async {
+                var fetchResponse = args[0];
+                print('Fetch response received: $fetchResponse');
+                try {
+                  Map<String, dynamic> jsonResponse = jsonDecode(fetchResponse);
+                  token = jsonResponse['token'];
+                  print('Token captured: $token');
+                } catch (e) {
+                  print('Error decoding response: $e');
+                }
+                return fetchResponse;
+              },
+            );
+          }
+          return fetchRequest;
+        },
+        onLoadError: (controller, url, code, message) {
+          print('Error loading page: $message');
+        },
+        onConsoleMessage: (controller, consoleMessage) {
+          print('Console message: ${consoleMessage.message}');
+        },
+      ),
     );
   }
 }
